f2cb82897ba5a685b0e59c5b7dc900521eab04c5
Roger <138773896+MR-Dragon1@users.noreply.github.com>
2025-11-04 22:31:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 header('Content-Type: application/json');
 
 $valid_user = "mrroger916";
-$valid_pass = md5("kopipagi");
+$valid_pass = md5("jasaseo");
 
 echo json_encode([
     "username" => $valid_user,
     "password" => $valid_pass
+
 ]);
